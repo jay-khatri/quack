024cc8fe72a8a678aacf264e9b030d6315669870
Jay Khatri <khatri.florida@gmail.com>
2018-10-24 14:39:14 -0500

    its sorta working

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { Form, Icon, Input, Button } from 'antd';
 
 const API_URL = process.env.API_URL;
 
@@ -10,6 +11,10 @@ class App extends Component {
     };
   }
 
+  hasErrors(fieldsError) {
+    return Object.keys(fieldsError).some(field => fieldsError[field]);
+  }
+
   componentDidMount() {
     const request_url = `${API_URL}` + "/test";
     console.log(request_url);
@@ -19,16 +24,56 @@ class App extends Component {
       .catch(err => console.log(err));
   }
 
+  handleSubmit(e) {
+    e.preventDefault();
+    this.props.form.validateFields((err, values) => {
+      if (!err) {
+        console.log('Received values of form: ', values);
+      }
+    });
+  }
+
   render() {
+    const { getFieldDecorator, getFieldsError, getFieldError, isFieldTouched } = this.props.form;
+
+    // Only show error after a field is touched.
+    const userNameError = isFieldTouched('userName') && getFieldError('userName');
+    const passwordError = isFieldTouched('password') && getFieldError('password');
     return (
-      <div className="App">
-      <header className="App-header">
-        <h1 className="App-title">Welcome to tamuhack's new registration thingy</h1>
-      </header>
-      <p className="App-intro">{this.state.response}</p>
-    </div>
+      <Form layout="inline" onSubmit={this.handleSubmit}>
+        <Form.Item
+          validateStatus={userNameError ? 'error' : ''}
+          help={userNameError || ''}
+        >
+          {getFieldDecorator('userName', {
+            rules: [{ required: true, message: 'Please input your username!' }],
+          })(
+            <Input prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />} placeholder="Username" />
+          )}
+        </Form.Item>
+        <Form.Item
+          validateStatus={passwordError ? 'error' : ''}
+          help={passwordError || ''}
+        >
+          {getFieldDecorator('password', {
+            rules: [{ required: true, message: 'Please input your Password!' }],
+          })(
+            <Input prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />} type="password" placeholder="Password" />
+          )}
+        </Form.Item>
+        <Form.Item>
+          <Button
+            type="primary"
+            htmlType="submit"
+            disabled={this.hasErrors(getFieldsError())}
+          >
+            Log in
+          </Button>
+        </Form.Item>
+      </Form>
     );
   }
 }
 
-export default App;
+export default Form.create()(App);
+
